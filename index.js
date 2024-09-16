1bdff4a16e71019759085f55b94533fdbfb06850
Ronnie Aristil <ronnie.aristil@gmail.com>
2024-09-16 13:56:06 -0700

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+// Declare customerName in global scope without let or const
+customerName = 'bob';
+
+// Function to uppercase the customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Declare bestCustomer globally without 'let'
+function setBestCustomer() {
+  bestCustomer = 'not bob';  // No 'let' means it's global
+}
+
+// Overwrite the best customer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Declare a constant leastFavoriteCustomer
+const leastFavoriteCustomer = 'some customer';
+
+// Attempt to change leastFavoriteCustomer (which will throw an error)
+function changeLeastFavoriteCustomer() {
+  // This will fail since leastFavoriteCustomer is a const
+  leastFavoriteCustomer = 'another customer';  
+}
